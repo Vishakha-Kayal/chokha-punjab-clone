9222131da7ae1bd432461694952034255252001e
Vishakha-Kayal <138288780+Vishakha-Kayal@users.noreply.github.com>
2024-06-19 23:46:37 +0530

    made changes in lock json file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1da1f2',
+      },
+      spacing: {
+        '128': '32rem',
+      },
+    },
   },
   plugins: [],
 }
